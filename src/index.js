1319ef9b58a57f91804af0271861b7bc2a88dcf9
Manjil Timsina <123795262+its-manzil@users.noreply.github.com>
2025-05-18 20:14:02 +0545

    changed

--- src/index.js
+++ src/index.js
@@ -7,23 +7,11 @@ import Home from "./pages/Customers/Home";
 import About from "./pages/Customers/About";
 import Contact from "./pages/Customers/Contact";
 import Cart from "./pages/Customers/Cart";
-import Store from "./pages/Customers/Store";
 import Login from "./pages/Customers/Login";
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import Checkout from "./pages/Customers/Checkout";
 import Profile from "./pages/Customers/Profile";
-import Dashboard from "./pages/Customers/Dashboard";
-import AdminDashboard from "./pages/Admin/AdminDashboard";
-import AdminSidebar from "./pages/Admin/AdminSideBar";
-import Products from "./pages/Admin/Products";
-import Settings from "./pages/Admin/Settings";
-import Orders from "./pages/Admin/Orders";
-import Categories from "./pages/Admin/Categories";
-import Reports from "./pages/Admin/Reports";
-import ContactMessages from "./pages/Admin/ContactMessages";
-import AdminLogin from "./pages/Admin/AdminLogin";
-import Stock from "./pages/Admin/Stock";
-
+import Store from "./pages/Customers/Store";
 
 
 
@@ -47,10 +35,6 @@ const router = createBrowserRouter([
     element: <Contact />,
   },
   {
-    path: "Dashboard",
-    element: <Dashboard/>,
-  },
-  {
     path: "Profile",
     element: <Profile />,
   },
@@ -63,56 +47,15 @@ const router = createBrowserRouter([
     element: <Checkout />,
   },
   
-  {
-    path: "Store",
-    element: <Store />,
-  },
+  
   {
     path: "Login",
     element: <Login />,
   },
   {
-    path: "AdminDashboard",
-    element: <AdminDashboard />,
+    path: "Store",
+    element: <Store/>
   },
-  
-  {
-    path: "AdminSideBar",
-    element: <AdminSidebar />,
-  },
-  {
-    path: "Products",
-    element: <Products />,
-  },
-  {
-    path: "Categories",
-    element: <Categories />,
-  },
-  {
-    path: "Orders",
-    element: <Orders />,
-  },
-    {
-    path: "Reports",
-    element: <Reports />,
-  },
-
-  {
-    path: "Settings",
-    element: <Settings />,
-  },
-  {
-    path: "ContactMessages",
-    element: <ContactMessages />,
-  },
-  {
-    path: "Admin",
-    element: <AdminLogin />,
-  },
-  {
-    path: "Stock",
-    element: <Stock />,
-  }
 
   
 ]);
